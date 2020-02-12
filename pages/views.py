99da0b2c1a38dae926b7633569a85905f822e8e9
B-Saidy <baijaw93@yahoo.com>
2020-02-12 23:04:16 +0800

    updated

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
+import os
+import requests
+API_KEY=os.environ.get('OWM_API')
+city = 'Banjul'
 from django.shortcuts import render
+# url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
+weather = {'temp': 21, 'feels_like': 20.4, 'temp_min': 21, 'temp_max': 21, 'pressure': 1011, 'humidity': 68}
 def home(request):
-    return render(request, 'pages/home.html')
+    # r = requests.get(url.format(city, API_KEY)).json()
+    # weather = r['main']
+    context = {
+        'weather':weather
+    }
+    return render(request, 'pages/home.html', context)
