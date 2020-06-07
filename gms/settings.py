e2fa15ae5b85b0653f4ccbc0c4f5d13b1907a8a1
B-Saidy <baijaw93@yahoo.com>
2020-06-07 18:37:16 +0800

    added allowed host

--- gms/settings.py
+++ gms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('GMS_DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://gam-met-services.herokuapp.com/']
 
 
 # Application definition
