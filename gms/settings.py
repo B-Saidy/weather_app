5d0b77f2c2fb4a386f0420abe5c8c1f4871fbca0
B-Saidy <baijaw93@yahoo.com>
2020-06-07 20:26:59 +0800

    changed settings

--- gms/settings.py
+++ gms/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('GMS_DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gam-met-services.herokuapp.com']
+ALLOWED_HOSTS = ['gammet.herokuapp.com', 'localhost']
 
 
 # Application definition
