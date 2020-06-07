886068f4ca320854f980e019214ca849a7eb4b77
B-Saidy <baijaw93@yahoo.com>
2020-06-07 22:58:03 +0800

    deleted static folder

--- gms/settings.py
+++ gms/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('GMS_DJANGO_SECRET_KEY')
-
+# SECRET_KEY = os.environ.get('GMS_DJANGO_SECRET_KEY')
+SECRET_KEY = 'wiojno$&e(y$sazgixrto!9+rrisg3ko3*acgueks(wu@uhpt_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
